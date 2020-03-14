9b0051e741117a180a920e82b6cf74f6f3961898
jhsm croonen <info@jcroonen.nl>
2020-03-14 11:00:21 +0100

    property active now can be saved

--- reader/options/options.js
+++ reader/options/options.js
@@ -11,14 +11,66 @@ function listStorage() {
     })
 }
 
+function updateStatus(msg) {
+    const status = document.getElementById('status');
+    status.textContent = msg;
+    setTimeout(function() {
+        status.textContent = '';
+        // window.close();
+    }, 4000);
+}
+
+function saveToStorage() {
+    const inputEls = document.querySelectorAll('#storagelist input');
+    getStorage(data => {
+        const entries = Object.entries(data);
+        for (const entry of entries) {
+            const [site, options] = entry;
+            for (let i = 0; i < inputEls.length; i++) {
+                if (inputEls[i].dataset.site === site) {
+                    options.active = inputEls[i].checked;
+                }
+            }
+        }
+        console.log(data);
+        chrome.storage.local.set(data, () => updateStatus('Options saved.'));
+    });
+}
+
+function createAnchor(value) {
+    const anchor = document.createElement('a');
+    anchor.href = 'https://' + value;
+    anchor.innerText = value;
+    anchor.setAttribute('target', '_blank');
+    return anchor;
+}
+
+function createCheckbox(site, checked) {
+    const inputEl = document.createElement('input');
+    inputEl.type = 'checkbox';
+    inputEl.dataset.site = site;
+    inputEl.checked = checked;
+    return inputEl;
+}
+
 function listSites(data, rlist, item) {
     const list = document.getElementById(rlist);
     const itemTemplate = document.getElementById(item);
+    // console.log('data', data);
     const entries = Object.entries(data);
-    // console.log('entries', entries);
+    console.log('entries', entries);
     for (const entry of entries) {
+        const site = entry[0];
+        const {active} = entry[1];
         const item = itemTemplate.cloneNode(false);
-        item.innerText = entry[0];
+        if (site[0] === '_') {
+            item.innerText = site;
+        } else {
+            const checkbox = createCheckbox(site, active);
+            item.appendChild(checkbox);
+            const anchor = createAnchor(site);
+            item.appendChild(anchor);
+        }
         list.appendChild(item);
     }
 }
@@ -44,7 +96,10 @@ function store(data, sendResponse) {
 
 function save() {
     getStorage(data => {
-        store(data, msg => console.log(msg));
+        store(data, msg => {
+            console.log(msg);
+            updateStatus(`options exported to ${jsonStorage.jsonfile}`);
+        });
     })
 }
 
@@ -71,15 +126,36 @@ function load() {
     })
 }
 
+function selectAll() {
+    const inputEls = Array.from(document.querySelectorAll('input'));
+    inputEls.forEach(function(inputEl) {
+        inputEl.checked = true;
+    });
+}
+
+function selectNone() {
+    const inputEls = Array.from(document.querySelectorAll('input'));
+    inputEls.forEach(function(inputEl) {
+        inputEl.checked = false;
+    });
+}
+
 function bindControls() {
     const bindings = [
-        ['save', save],
-        ['load', load],
+        ['save', saveToStorage],
+        ['export', save],
+        ['import', load],
         ['restore', restore],
+        ['select-all', selectAll],
+        ['select-none', selectNone],
     ];
     for (const [id, fun] of bindings) {
-        document.getElementById(id)
-            .addEventListener('click', () => fun());
+        const element = document.getElementById(id);
+        if (element) {
+            element.addEventListener('click', () => fun());
+        } else {
+            console.error('No element found with id=' + id);
+        }
     }
 }
 
