921a8d51522d4717f15dff08eb2a95e3d3302865
jan croonen <info@jcroonen.nl>
2024-03-05 19:12:15 +0100

    storage overview can update active field

--- reader/options/storage/storage.js
+++ reader/options/storage/storage.js
@@ -1,10 +1,59 @@
-function init() {
-    // const storageDump = document.getElementById('storage-dump');
-    chrome.storage.local.get(null, sites => {
-        // listSites(sites, 'storagelist');
-        console.log(sites);
-    })
+let sites = null;
 
+function updateSite(input) {
+    const li = input.parentElement;
+    const name = li.querySelector('span').innerText.trim();
+    for (const [key, value] of Object.entries(sites)) {
+        if (key === name) {
+            value.active = input.checked ? 'on' : 'off';
+            break;
+        }
+    }
+}
+
+function makeCheckbox(value) {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'checkbox');
+    if (value.active === 'on') {
+        input.setAttribute('checked', 'checked');
+    }
+    input.addEventListener('change', e => {
+        if (sites) {
+            updateSite(e.target);
+            chrome.storage.local.set(sites, () => console.log('options updated.'));
+        }
+    })
+    return input;
+}
+
+function makeCaption(name) {
+    const span = document.createElement('span');
+    span.innerText = ' ' + name;
+    return span;
+}
+
+function siteLi(name, value) {
+    const li = document.createElement('li');
+    li.appendChild(makeCheckbox(value));
+    li.appendChild(makeCaption(name));
+    return li;
+}
+
+function listSites(sites) {
+    const sitesList = document.getElementById('sites-list');
+    for (const [key, value] of Object.entries(sites)) {
+        if (key.length > 0) {
+            sitesList.appendChild(siteLi(key, value));
+        }
+    }
+}
+
+function init() {
+    chrome.storage.local.get(null, sites1 => {
+        listSites(sites1, 'storagelist');
+        // console.log(sites1);
+        sites = sites1;
+    })
 }
 
 init();
