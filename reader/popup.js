8f8b6276a176ab255d21900161417ef9bc068425
jan croonen <info@jcroonen.nl>
2024-03-22 10:12:01 +0100

    Update popup.js
    vermijd editors venster openen als het venster al open is

--- reader/popup.js
+++ reader/popup.js
@@ -1,6 +1,8 @@
 import {background} from "./background/backgroundState.js";
 import {toggleDarkSettings, toggleGeneralSettings} from "./shared/popuplib.js";
 
+let openedWinId = -1;
+
 function createWin(curWin) {
     chrome.windows.create({
         url: 'popup/popup.html',
@@ -10,18 +12,43 @@ function createWin(curWin) {
         top: curWin.top,
         left: curWin.left - 500
     }, win => {
+        openedWinId = win.id;
         background.winId = win.id;
         background.tTabId = win.tabs[0].id;
     })
 }
 
-function openWin() {
+function isWindowExistent(windowId) {
+    return new Promise((resolve, reject) => {
+        chrome.windows.get(windowId, (window) => {
+            if (chrome.runtime.lastError) {
+                // Window not found, likely doesn't exist
+                resolve(false);
+            } else {
+                // Window found, it exists
+                resolve(true);
+            }
+        });
+    });
+}
+
+function _openWin() {
     chrome.tabs.query({
         active: true
     }, tabs => {
         chrome.windows.get(tabs[0].windowId, curWin => {
             createWin(curWin);
         })
+    })
+}
+
+function openWin() {
+    if (openedWinId === -1) {
+        _openWin();
+        return;
+    }
+    isWindowExistent(openedWinId).then(r => {
+        if (!r) _openWin();
     })
 }
 
