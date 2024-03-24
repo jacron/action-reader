0566aef388f7384e65facd6dd8531f7be3cec1ad
Jan Croonen <info@jcroonen.nl>
2024-03-24 23:35:31 +0100

    na verse install op Luna

--- reader/background/openEditors.js
+++ reader/background/openEditors.js
@@ -1,13 +1,17 @@
 import {background} from "./backgroundState.js";
 
 function createWin(curWin) {
+    let left = curWin.left - 500;
+    if (left < 0) {
+        left = 0;
+    }
     chrome.windows.create({
         url: 'popup/popup.html',
         type: 'popup',
         width: 500,
         height: curWin.height,
         top: curWin.top,
-        left: curWin.left - 500
+        left
     }, win => {
         background.winId = win.id;
         background.tTabId = win.tabs[0].id;
