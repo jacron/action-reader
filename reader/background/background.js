2bc3e0ca4c2645962a5173f9ae69fe2072efb57c
jan croonen <info@jcroonen.nl>
2024-04-17 18:19:30 +0200

    Update background.js
    fixed: open window toggle

--- reader/background/background.js
+++ reader/background/background.js
@@ -1,6 +1,7 @@
 import {updateBadge} from "./badge.js";
 import {messageToContent} from "../shared/popuplib.js";
 import {closeEditors, openEditors} from "./openEditors.js";
+import {background} from "./backgroundState.js";
 
 function activateListener(activeInfo) {
     chrome.tabs.get(activeInfo.tabId, activeTab => {
@@ -33,6 +34,13 @@ function commandListener(command) {
     }
 }
 
+function windowRemoved(winId) {
+    if (background.winId === winId) {
+        background.winId = null;
+    }
+}
+
 chrome.tabs.onUpdated.addListener(updateListener);
 chrome.tabs.onActivated.addListener(activateListener);
 chrome.commands.onCommand.addListener(commandListener);
+chrome.windows.onRemoved.addListener(windowRemoved);
