bdfce75e1f1f2d4044978a237a63bc8acbad6c7a
jan croonen <info@jcroonen.nl>
2024-04-18 06:35:22 +0200

    Update switches.js
    herstel simpele switches

--- reader/popup/switches.js
+++ reader/popup/switches.js
@@ -1,21 +1,4 @@
-const STORAGE = chrome.storage.local;
-const KEY_BOOLEAN_CUSTOM_READER = 'readerOn';
-const KEY_BOOLEAN_CUSTOM_DARK = 'darkOn';
-
-function undefinedOrTrue(x) {
-    return x === undefined || x === true;
-}
-
-function setSwitch(switchElement, state) {
-    const classList = switchElement.classList;
-    if (state) {
-        classList.remove('off');
-        classList.add('on');
-    } else {
-        classList.remove('on');
-        classList.add('off');
-    }
-}
+import {messageToContent} from "../shared/popuplib.js";
 
 function toggleOnOff(classList, cb) {
     if (classList.contains('on')) {
@@ -29,20 +12,18 @@ function toggleOnOff(classList, cb) {
     }
 }
 
-function initOneSwitch(_activeHost, custom, switchId, storageKey) {
+function initOneSwitch(_activeHost, custom, switchId, message) {
     const _switch = document.getElementById(switchId);
-    setSwitch(_switch, undefinedOrTrue(custom[storageKey]));
     _switch.onclick = (e) => {
-        toggleOnOff(e.target.classList, state => {
-            custom[storageKey] = state;
-            STORAGE.set({[_activeHost]: custom}).then();
+        toggleOnOff(e.target.classList, () => {
+            messageToContent({message})
         })
     }
 }
 
 function initSwitches(_activeHost, custom) {
-    initOneSwitch(_activeHost, custom, 'dark-toggle-switch', KEY_BOOLEAN_CUSTOM_DARK);
-    initOneSwitch(_activeHost, custom, 'general-toggle-switch', KEY_BOOLEAN_CUSTOM_READER);
+    initOneSwitch(_activeHost, custom, 'dark-toggle-switch', 'toggleDark');
+    initOneSwitch(_activeHost, custom, 'general-toggle-switch', 'toggleReader');
 }
 
 export {initSwitches}
