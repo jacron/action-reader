bae82bcf19f75d8398997775d87fbd9c4a15f53b
jan croonen <info@jcroonen.nl>
2025-08-10 15:52:19 +0200

    use puppeteer (in service)

--- reader/content/fetchArchiveContent.js
+++ reader/content/fetchArchiveContent.js
@@ -1,44 +1,30 @@
-function lastThumbBlockItemHref(doc) {
-    const divs = doc.querySelectorAll('.THUMBS-BLOCK > div');
-    if (divs.length) {
-        const lastDiv = divs[divs.length - 1];
-        if (lastDiv) {
-            return lastDiv.querySelector('a');
-        }
-    }
-    return false;
-}
-
 function isNotGlobalSite(url) {
     const w = url.split('/');
     return w.length > 4;
 }
 
 export function fetchArchiveContent(url) {
-    fetch(url)
-        .then(response => response.text())
-        .then(html => {
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(html, 'text/html');
-            const lastHref = lastThumbBlockItemHref(doc);
-            if (lastHref) {
-                if (doc.forms[0]) {
-                    const referer = doc.forms[0].querySelector('input[name=q]').value;
+    fetch(`http://localhost:3099/archive-thumbs?url=${encodeURIComponent(url)}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(`*** Found ${data.count} thumbnails`);
+            console.log(data);
+            const {thumbLinks, referer} = data;
+            if (thumbLinks && thumbLinks.length > 0) {
+                const lastHref = thumbLinks[thumbLinks.length - 1];
+                if (referer) {
+                    console.log('*** Found referer:', referer);
                     if (isNotGlobalSite(referer)) {
-                        console.log('*** Redirecting to last thumb block item:', lastHref.href);
-                        document.location.replace(lastHref.href);
-                    } else {
-                        return null;
+                        console.log('*** Redirecting to last thumb block item:', lastHref);
+                        document.location.replace(lastHref);
                     }
                 }
-                else {
-                    return null;
-                }
+            } else {
+                console.log('*** No last thumb block item found');
+                document.location.replace(url);
             }
-            console.log('*** No last thumb block item found');
-            document.location.replace(url);
         })
         .catch(error => {
-            console.error('Error fetching archive:', error);
+            console.error('Error fetching archive via Puppeteer API:', error);
         });
 }
